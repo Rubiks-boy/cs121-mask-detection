80349f9c299ffdd03005201aaf534d6bd261aa3e
Xander Hirsch <ahirsch@hmc.edu>
2021-03-12 21:37:26 -0800

    Restructrued flask source directory

--- flask/app.py
+++ flask/app.py
@@ -2,9 +2,38 @@ from flask import Flask, request, send_from_directory
 from markupsafe import escape
 from werkzeug.utils import secure_filename
 import os.path
+import json
 app = Flask(__name__)
 STATIC = 'static'
 UPLOADS = 'uploads'
+
+DEFAULT_RESPONSE = json.loads(
+    """
+    [
+    {
+        "top": "14",
+        "bottom": "71",
+        "left": "6.5",
+        "right": "23",
+        "result": 0
+    },
+    {
+        "top": "10",
+        "bottom": "67",
+        "left": "42",
+        "right": "58",
+        "result": 1
+    },
+    {
+        "top": "4",
+        "bottom": "67",
+        "left": "74.5",
+        "right": "93",
+        "result": 2
+    }
+    ]
+    """
+)
 
 @app.route('/')
 def index():
@@ -13,6 +42,7 @@ def index():
 
 @app.route('/test', methods=['POST'])
 def upload_text():
+    print(f'This is our request:\n{request.form}')
     text_returned = []
     for key in request.form.keys():
         text_returned.append(f'{key} -> {request.form[key]}')

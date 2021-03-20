d2186ca36c6154e5e0461dc7e99b41aa26c29ebb
McGarvs <47725552+McGarvs@users.noreply.github.com>
2021-03-20 13:27:44 -0700

    Model connects to fronend

--- demo.py
+++ demo.py
@@ -5,9 +5,16 @@ from datetime import datetime as dt
 # from markupsafe import escape
 # from werkzeug.utils import secure_filename
 # import os.path
+from fastai.learner import load_learner
 import json
 import sys
 import os
+## Imports to run on Windows correctly
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+#######################################
+
 
 app = Flask(__name__)
 # TODO: we're allowing access from any origin
@@ -58,6 +65,20 @@ RESP_GOOD_MASK, RESP_INCORRECT_MASK, RESP_NO_MASK = [
     cropped_resp(x) for x in (2, 1, 0)
 ]
 
+def make_prediction(image_path):
+
+    model = load_learner("face_mask_classifier.pkl", cpu=True)
+    prediction = model.predict(image_path)
+    mask_class = prediction[0]
+
+    if mask_class == "without_mask":
+        return RESP_NO_MASK
+    elif mask_class == "with_mask":
+        return RESP_GOOD_MASK
+    elif mask_class == "mask_weared_incorrect":
+        return RESP_INCORRECT_MASK
+    else:
+        return "AAAAAHHHH"
 
 @app.route('/')
 def index():
@@ -78,11 +99,12 @@ def cropped_face():
     image = dict(request.files.lists())["file"][0]
     # print("Printing the image:", image, type(image), file=sys.stderr)
     cwd = os.path.dirname(os.path.realpath(__file__))
-    print("dir: ", cwd, file=sys.stderr)
-    image.save(os.path.join(cwd, "upload", "test.png"))
-    # image = request.files[0]
+    # print("dir: ", cwd, file=sys.stderr)
+    image_path = os.path.join(cwd, "upload", "my_upload.png")
+    image.save(image_path)
+    prediction = make_prediction(image_path)
+    print("Model Prediction: ", prediction, file=sys.stderr)
+    return Response(prediction)
+    # Is this arg necessary???
+    # mimetype='application/json'
 
-    # Use the seconds of the minute as a placeholder "detection"
-    # 0=no mask, 1=mask incorrect, 2=mask worn correctly
-    status = (dt.now().second // 20)
-    return Response(cropped_resp(status), status=200, mimetype='application/json')

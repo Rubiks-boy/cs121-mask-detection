74176d9c6b9ef590051394003417d4a0f3882ee4
Xander Hirsch <ahirsch@hmc.edu>
2021-04-15 14:23:21 -0700

    Fixed uploading by clicking on dropzone

--- react/src/components/upload/UploadImage.js
+++ react/src/components/upload/UploadImage.js
@@ -2,38 +2,43 @@ import axios from 'axios'
 
 const maskAPI = 'https://facee-309423.wl.r.appspot.com'
 
+function uploadImage(formData, updateMasks, setLoading) {
+    // eslint-disable-next-line no-undef
+    const queryParams = new URLSearchParams(window.location.search)
+    const endpoint = queryParams.get('endpoint')
+
+    // After loading true until a response is back
+    setLoading(true)
+
+    axios
+        .post(`${maskAPI}/${endpoint || 'detect'}`, formData)
+        .then((res) => {
+            // then print response status
+            console.log('Uploaded from Axios')
+            console.log(res.data)
+            updateMasks(res.data)
+            setLoading(false)
+        })
+        .catch((err) => {
+            console.log('rip')
+            console.log(err)
+            setLoading(false)
+        })
+}
+
 export default function handleUpload(updateImage, updateMasks, setLoading) {
     function handleChange(e) {
-        const imgFile = e[0]
+        // typeof e is diffrent between click upload and drag-n-drop
+        const imgFile = 'target' in e ? e.target.files[0] : e[0]
         const imgBlob = URL.createObjectURL(imgFile)
         updateMasks([])
         updateImage(imgBlob)
-
-        // After loading true until a response is back
-        setLoading(true)
 
         // eslint-disable-next-line no-undef
         const data = new FormData()
         data.append('file', imgFile)
 
-        // eslint-disable-next-line no-undef
-        const queryParams = new URLSearchParams(window.location.search)
-        const endpoint = queryParams.get('endpoint')
-
-        axios
-            .post(`${maskAPI}/${endpoint || 'detect'}`, data)
-            .then((res) => {
-                // then print response status
-                console.log('Uploaded from Axios')
-                console.log(res.data)
-                updateMasks(res.data)
-                setLoading(false)
-            })
-            .catch((err) => {
-                console.log('rip')
-                console.log(err)
-                setLoading(false)
-            })
+        uploadImage(data, updateMasks, setLoading)
     }
     return handleChange
 }

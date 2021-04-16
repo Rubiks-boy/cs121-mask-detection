c29f5c6c8c8872a925bafa49a457afcffceb9976
Xander Hirsch <ahirsch@hmc.edu>
2021-04-15 18:06:56 -0700

    Moved external CSS link

--- react/src/components/MaskApp.js
+++ react/src/components/MaskApp.js
@@ -6,16 +6,9 @@ import Upload from './upload/Upload'
 export default function MaskApp() {
     const [image, updateImage] = useState(null)
     const [masks, updateMasks] = useState([])
-    // eslint-disable-next-line
     const [loading, setLoading] = useState(false)
     return (
         <Container component="main" maxWidth="lg">
-            <link
-                rel="stylesheet"
-                href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-                integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-                crossOrigin="anonymous"
-            />
             <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
                 <Grid item xs={12}>
                     <Upload

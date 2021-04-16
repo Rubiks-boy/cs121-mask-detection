e383b00ff012842fab623b6c86984f4bde763071
Adam Walker <adamwalker567123@gmail.com>
2021-04-15 23:53:54 -0700

    Header from a template

--- react/src/App.js
+++ react/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './App.css'
 import MaskApp from './components/MaskApp'
+import Header from './components/Header'
 
 function App() {
-    return <MaskApp />
+    return (
+        <>
+            <Header />
+            <MaskApp />
+        </>
+    )
 }
 
 export default App
--- react/src/components/MaskApp.js
+++ react/src/components/MaskApp.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react'
-import { Container, Grid } from '@material-ui/core'
+import { Container, Grid, makeStyles } from '@material-ui/core'
 import MaskResults from './image/MaskResults'
 import Upload from './upload/Upload'
 
+const useStyles = makeStyles((theme) => ({
+    content: {
+        padding: theme.spacing(4),
+    },
+}))
+
 export default function MaskApp() {
+    const classes = useStyles()
+
     const [image, updateImage] = useState(null)
     const [masks, updateMasks] = useState([])
     const [loading, setLoading] = useState(false)
     return (
-        <Container component="main" maxWidth="lg">
+        <Container component="main" maxWidth="lg" className={classes.content}>
             <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
                 <Grid item xs={12}>
                     <Upload

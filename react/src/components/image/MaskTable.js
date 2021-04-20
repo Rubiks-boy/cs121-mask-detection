a5b5b173488be792956b07472caeee98e99ef6c0
ntan9 <nicktan252@gmail.com>
2021-04-19 21:47:59 -0700

    Added info alert when no faces are detected in an image

--- react/src/components/image/MaskTable.js
+++ react/src/components/image/MaskTable.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles'
 import Container from '@material-ui/core/Container'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
@@ -8,7 +9,15 @@ import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 import Checkbox from '@material-ui/core/Checkbox'
+import Box from '@material-ui/core/Box'
+import Alert from '@material-ui/lab/Alert'
 import { withStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+    alert: {
+        spacing: theme.spacing(8),
+    },
+}))
 
 const getCheckboxColor = (theme, i) => {
     const colors = [theme.palette.error, theme.palette.warning, theme.palette.success]
@@ -48,9 +57,14 @@ const MaskTableRow = (row, index) => {
     )
 }
 
-export default function MaskTable({ boxes, showCat, onCheckEvent }) {
+export default function MaskTable({ boxes, showCat, onCheckEvent, loading, image }) {
+    const classes = useStyles()
+
     const names = ['No Mask', 'Incorrect Mask', 'Mask']
     const nums = [0, 1, 2].map((i) => boxes.filter((x) => x.result === i).length)
+
+    const zero = nums.reduce((a, b) => a + b, 0) === 0
+    const noFaceAlert = !image && !loading && zero
 
     const rows = [0, 1, 2].map((i) => {
         return {
@@ -63,6 +77,13 @@ export default function MaskTable({ boxes, showCat, onCheckEvent }) {
 
     return (
         <Container component="mask-detector-results">
+            <Box mb={1}>
+                {noFaceAlert && (
+                    <Alert severity="info" className={classes.alert}>
+                        No Faces Detected
+                    </Alert>
+                )}
+            </Box>
             <TableContainer component={Paper}>
                 <Table aria-label="simple table">
                     <TableHead>
